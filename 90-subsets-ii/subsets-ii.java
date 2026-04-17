410b98c0f26e27ab6c083a5663a954f0dc00153a
KhushiJindal09 <jindalkhushi0908@gmail.com>
2026-04-17 18:11:53 +0530

    Time: 3 ms (56.54%) | Memory: 45.3 MB (28.02%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        helper(nums, 0, new ArrayList<>(), ans);
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
-    public void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(curr));
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) continue;
-            curr.add(nums[i]);
-            helper(nums, i + 1, curr, ans);
-            curr.remove(curr.size() - 1);
+    void backtrack(int[] nums, int index, List<Integer> path, List<List<Integer>>ans) {
+        ans.add(new ArrayList<>(path));
+        for (int i = index; i<nums.length; i++) {
+            if (i>index && nums[i] == nums[i-1]) continue;
+            path.add(nums[i]);
+            backtrack(nums, i+1, path, ans);
+            path.remove(path.size()-1);
         }
     }
 }
