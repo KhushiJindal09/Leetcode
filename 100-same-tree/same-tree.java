8cc885842f647fd8575507b645accf76fbf16d5d
KhushiJindal09 <jindalkhushi0908@gmail.com>
2026-05-13 16:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (51.74%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -18,6 +18,8 @@ class Solution {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
         if(p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return left && right;
     }
 }
