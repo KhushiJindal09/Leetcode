93f319bcc678971feab5acb1dea122dc842b2121
Khushi <jindalkhushi0908@gmail.com>
2026-07-04 22:46:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (95.42%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isThree(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                    count++;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count == 3;
+    }
+}
